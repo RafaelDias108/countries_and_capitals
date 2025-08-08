0f01d9e834c7bd14d8e36225bee17544f6ff87ff
RafaelDias108 <rafaelsd70@gmail.com>
2025-08-07 23:22:41 -0300

    Ajuste no botão cancelar jogo

--- resources/views/game.blade.php
+++ resources/views/game.blade.php
@@ -10,8 +10,9 @@
         </div>
 
     </div>
+
+    <!-- cancel game -->
+    <div class="text-center mt-5">
+        <a href="{{ route('start_game') }}" class="btn btn-outline-danger mt-3 px-5">CANCELAR JOGO</a>
+    </div>
 </x-main-layout>
-<!-- cancel game -->
-<div class="text-center mt-5">
-    <a href="{{ route('start_game') }}" class="btn btn-outline-danger mt-3 px-5">CANCELAR JOGO</a>
-</div>
