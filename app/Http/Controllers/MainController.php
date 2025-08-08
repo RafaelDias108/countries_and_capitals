5a42e369368735a2c8f01140bad65bde62d40599
RafaelDias108 <rafaelsd70@gmail.com>
2025-08-07 22:45:27 -0300

    Avançar no Jogo

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -152,4 +152,26 @@ class MainController extends Controller
 
         return view('answer_result')->with($data);
     }
+
+    public function nextQuestion()
+    {
+        $current_question = session('current_question');
+        $total_questions = session('total_questions');
+
+        // check if the game is over
+        if($current_question < $total_questions) {
+            $current_question++;
+            session()->put('current_question', $current_question);
+            return redirect()->route('game');
+        } else {
+            // game over
+            return redirect()->route('show_results');
+        }
+    }
+
+    public function showResults()
+    {
+        echo "mostrar resultados finais";
+        dd(session()->all());
+    }
 }
